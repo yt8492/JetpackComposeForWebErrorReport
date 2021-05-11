af88e06f97117d412c411653669bf7810de02657
yt8492 <tmymyut2000@gmail.com>
2021-05-12 02:16:12 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "JetpackComposeForWebErrorReport"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+import androidx.compose.web.elements.Button
+import androidx.compose.web.renderComposable
+
+fun main() {
+    renderComposable(rootElementId = "root") {
+        Button {
+        }
+    }
+}
